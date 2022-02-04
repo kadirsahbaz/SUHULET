ff4a3577069ce3a0d91c53290a3ca55002b8a47f
Kadir Şahbaz <kadirsahbaz@hotmail.com>
2022-02-04 23:01:31 +0300

    added docstrings

--- SUHULET.py
+++ SUHULET.py
@@ -1,18 +1,57 @@
-from qgis.core import (QgsProject,
-                       QgsMapLayer,
-                       QgsLayerTreeGroup,
-                       QgsLayerTreeLayer)
+# -*- coding: utf-8 -*-
+"""
+*******************************************************
+                       SUHULET
+                                 
+Easy accsee to liayer and group in QGIS Layers panel
+
+                              -------------------
+        begin                : 2022-01-31
+        author               : Kadir Şahbaz
+        email                : kadirsahbaz@htomail.com
+ *******************************************************
+
+Sample Layer Tree:
+
+A/                  Group
+├─ LayerA           Layer
+├─ B/               Group
+│  ├─ LayerB        Layer
+│  ├─ C/            Group
+│  │  ├─ LayerC     Layer
+│  │  ├─ LayerE     Layer
+│  ├─ D/            Group
+│  │  ├─ LayerD     Layer
+
+G() -> returns root
+G("A") -> group A
+G("A/B") -> group B
+
+Gs() -> returns all group in root
+Gs("A/B") returns [group C, group D]
+
+L("Layer") -> returns the first layer (QgsMapLayer) named "Layer"   
+              in layer tree. It is recursive. 
+L("A/B/C/LayerC") -> LayerC (returns QgsMapLayer, not QgsLayerTreeLayer)
+L("/A/B/C/LayerC/") -> the same above. Ignores leading and trailing "/"               
+L("A/B/C") -> rises an error. The last item have to be a QgsMapLayer.
+L("A/B/C")
+
+Ls() -> all root layers, except groups
+Ls("A/B/C") -> [LayerC, LayerE] - QgsMapLayers
+"""
+
+from qgis.core import (QgsProject, QgsMapLayer, QgsLayerTreeGroup, QgsLayerTreeLayer)
 
 
 def L(layer_path):
     """ Return the layer given in path
-    Example:
-    Layer("group/subgroup/layer")
+    Example: Layer("group/subgroup/layer")
     """
 
     layer = None
     group = QgsProject.instance().layerTreeRoot()
-    items =  layer_path.split('/')
+    items =  layer_path.strip("/").split('/')
     if len(items) == 1:
         try:
             layer = QgsProject.instance().mapLayersByName(items[0])[0]
@@ -50,7 +89,7 @@ def G(group_path: str = None):
     group = QgsProject.instance().layerTreeRoot()
 
     if group_path:
-        groups =  group_path.split('/')
+        groups =  group_path.strip("/").split('/')
         for g in groups:
             group = group.findGroup(g)
             if not group:
